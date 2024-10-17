ab880fa0f443b56368dab4bbd06a1df90a7caad4
iii80 <myfriends123@gmail.com>
2024-10-17 17:37:13 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -6,7 +6,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #临时使用 SSR+ 自用修改源
 # git clone https://github.com/iii80/helloworld.git;master package/helloworld
-echo "src-git helloworld https://github.com/iii80/helloworld.git;master" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
 
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
